aaf42b53a2df331b7ecf3f4a4feeccdb4261541b
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-09-12 11:53:33 +0530

    User synced to db

--- mobile/utils/api.ts
+++ mobile/utils/api.ts
@@ -3,6 +3,7 @@ import { useAuth } from "@clerk/clerk-expo";
 
 const API_BASE_URL = "https://x-clone-rn-alpha-sooty.vercel.app/api";
 
+// This will create an authenticated api
 export const createApiClient = (getToken: () => Promise<string|null>):AxiosInstance => {
     
     const api = axios.create({ baseURL:API_BASE_URL });
