c63d2ed9ca3ae9ed202aa9e78a742dfe75409d72
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-09-15 12:53:40 +0530

    Changed things to how they were. Gonna create a new branch, to test out things

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,8 +14,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use(clerkMiddleware());
-// app.use(arcjetMiddleware);
+app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
 
 app.get("/", (req, res) => res.send("Hello from server"));
 
